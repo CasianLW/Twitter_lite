a8bc485b873e523802d8ede92025c677387c371c
CasianLW <ciorbacasian@gmail.com>
2023-04-02 18:35:30 +0200

    better belongs to

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tweet extends Model
 {
-    public function user(){ return $this->belongsTo(User::class);}
+    public function user(): BelongsTo
+    { return $this->belongsTo(User::class);}
+    
     use HasFactory;
 
     protected $fillable = [
