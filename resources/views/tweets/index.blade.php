a026e8982aa412fe28ed07df1dd1b2b2f1ca8e97
CasianLW <ciorbacasian@gmail.com>
2023-04-02 19:11:04 +0200

    position & margin to edit menu

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -12,7 +12,9 @@
         </form>
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
             @foreach ($tweets as $tweet)
+            
                 <div class="p-6 flex space-x-2">
+                    
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                     </svg>
@@ -25,11 +27,10 @@
                                 </div>
                                 <span class="ml-2 text-sm text-gray-600"> {{ $tweet->created_at->diffForHumans() }}</span>
                             </div>
-                            
-                                @if ($tweet->user->is(auth()->user()))
-                                <x-dropdown>
+                            @if ($tweet->user->is(auth()->user()))
+                                <x-dropdown >
                                     <x-slot name="trigger">
-                                        <button>
+                                        <button class="ml-2">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                                                 <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                             </svg>
@@ -42,13 +43,16 @@
                                     </x-slot>
                                 </x-dropdown>
                             @endif
+                               
                         </div>
+                        
                         <p class="mt-4 text-lg text-gray-900">{{ $tweet->message }}</p>
                         @unless ($tweet->created_at->eq($tweet->updated_at))
                                     <small class="relative bottom-0 text-sm text-gray-600"> &middot; {{ __('Modifié ') }} {{ $tweet->updated_at->diffForHumans() }}</small>
                                 @endunless
                     </div>
                 </div>
+                
             @endforeach
         </div>
     </div>
